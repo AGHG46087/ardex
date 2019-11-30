d77129cc491fda9fb6f51f8ab3c9dffe70bb8df5
Hans-Jurgen Greiner <hgrein@2019-iMac.lan>
2019-11-30 08:09:46 -0700

    Initial checkin of C file

---
+++ T05_Multi_RGB_Driver/T05_Multi_RGB_Driver.ino
@@ -0,0 +1,52 @@
+//www.elegoo.com
+//2016.12.9 
+
+int tDelay = 200;
+int latchPin = 11;      // (11) ST_CP [RCK] on 74HC595
+int clockPin = 9;      // (9) SH_CP [SCK] on 74HC595
+int dataPin = 12;     // (12) DS [S1] on 74HC595
+
+byte leds = 0;
+bool up = true;
+
+void updateShiftRegister()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
+
+void setup() 
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+  delay(tDelay);
+}
+
+void loop() 
+{
+  leds = 0; 
+  updateShiftRegister();
+  for (int i = 0; i < 8; i++)
+  {
+    bitSet(leds, i);
+    updateShiftRegister();
+    delay(tDelay);
+  }
+  for (int i = 0; up && i < 8; i++)
+  {
+    bitClear(leds, i);
+    updateShiftRegister();
+    delay(tDelay);
+  }
+  for (int i = 8; !up && i > -1; i--)
+  {
+    bitClear(leds, i);
+    updateShiftRegister();
+    delay(tDelay);
+  }
+
+  up = !up;
+  
+}
