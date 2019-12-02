0a3a3c2f872c966e7a4ef03de3d6c4c7e06a2792
Hans-Jurgen Greiner <hgrein@2019-iMac.lan>
2019-12-02 06:09:26 -0700

    Inital checkin of C module

---
+++ T06_MakeSounds/T06_MakeSounds.ino
@@ -0,0 +1,27 @@
+//www.elegoo.com
+//2016.12.08
+
+#define BUZZER 12 //the pin of the active buzzer
+
+void setup()
+{
+ pinMode(BUZZER,OUTPUT);//initialize the buzzer pin as an output
+}
+void loop()
+{
+ unsigned char i;
+   //output an frequency
+   for(i=0;i<80;i++) {
+    digitalWrite(BUZZER,HIGH);
+    delay(1);//wait for 1ms
+    digitalWrite(BUZZER,LOW);
+    delay(1);//wait for 1ms
+   }
+    //output another frequency
+   for(i=0;i<100;i++) {
+     digitalWrite(BUZZER,HIGH);
+     delay(2);//wait for 2ms
+     digitalWrite(BUZZER,LOW);
+     delay(2);//wait for 2ms
+   }
+} 
